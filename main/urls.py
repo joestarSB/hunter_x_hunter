08fabdfb384e2336dda58577eca627d6a843728e
unknown <you@example.com>
2023-04-10 18:19:39 +0300

    init

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import index, get_gems
+from .views import get_gems, get_gem
 urlpatterns = [
-    path('', get_gems, name='index'),
-    path('gems/<int:gem_id>', get_gems, name='gems'),
+    path('gems/', get_gems, name='index'),
+    path('gems/<int:gem_id>', get_gem, name='gem'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Gems
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -11,3 +12,8 @@ def index(request):
 def get_gems(request):
     gems = Gems.objects.all()
     return render(request, 'main/index.html', context={'gems': gems})
+
+
+def get_gem(request, gem_id):
+    gem = get_object_or_404(Gems, pk=gem_id)
+    return render(request, 'main/store.html', context={'gem': gem})
